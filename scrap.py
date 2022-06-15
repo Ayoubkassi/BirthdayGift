ee1b34dc8f59788fefd7c5dfb14d73751c0dc32d
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-06-15 12:36:26 +0100

    change website and method of working

--- scrap.py
+++ scrap.py
@@ -4,16 +4,30 @@ from time import sleep
 from datetime import datetime
 import subprocess
 import shlex
+import pyautogui
+import pyperclip
 
-music = input("Enter ur favourite music : ")
+values = []
+items= {}
+
+# music = input("Enter ur favourite music : ")
+# values.append(music)
 food = input("Enter ur favourite food : ")
+values.append(food)
 movie = input("Enter ur favourite movie  : ")
+values.append(movie)
 habbit = input("Enter ur favourite habbit : ")
+values.append(habbit)
 country = input("Enter ur favourite countrie : ")
+values.append(country)
 animal = input("Enter ur favourite animal : ")
+values.append(animal)
 singer = input("Enter ur favourite singer : ")
+values.append(singer)
 thing1 = input("Enter ur favourite thing 1 : ")
+values.append(thing1)
 thing2 = input("Enter ur favourite thing 2 : ")
+values.append(thing2)
 
 
 now = datetime.now()
@@ -22,143 +36,38 @@ begin_time = now.strftime("%H:%M:%S")
 print(begin_time)
 
 
+def getUrl(name):
+    webdriver.Chrome().get("https://images.google.com/")
+    sleep(2)
+    login_field = webdriver.Chrome().find_element_by_xpath(
+        '//*[@id="sbtc"]/div/div[2]/input').send_keys(name+" image size : 886x1920")
+    next_butt = webdriver.Chrome().find_element_by_xpath(
+        '//*[@id="sbtc"]/button').click()
 
-class BirthdayGift:
-    def __init__(self, music, food , movie , habbit , country , animal , singer , thing1 , thing2):
-        self.driver = webdriver.Chrome()
-
-        #get food
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(food)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-        sleep(3)
-
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-
-        #get movie
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(movie)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
+    pyautogui.moveTo(200, 510, duration = 1)
+    pyautogui.click()
+    sleep(1)
+    pyautogui.moveTo(800, 510, duration = 1)
+    sleep(3)
+    pyautogui.click(button='right')
+    pyautogui.press('up', presses=3)
+    pyautogui.press('enter')
+    url =  pyperclip.paste()
+    return url
 
 
-        #get habbit
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(habbit)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
+#get all necessary links
+for item in values:
+    items[item] = getUrl(item)
 
 
-        #get country
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(country)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
-
-
-        #get animal
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(animal)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
-
-
-        #get singer
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(singer)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-
-        sleep(3)
-
-        #get thing1
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(thing1)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
-
-
-        #get thing2
-        self.driver.get("https://picjumbo.com/")
-        sleep(2)
-        login_field = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[1]').send_keys(thing2)
-        next_butt = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/form/input[2]').click()
-        image = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div[1]/a/picture/img').click()
-
-        sleep(2)
-        download = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/article/div/div[4]/a[1]').click()
-        sleep(3)
+# class BirthdayGift:
+#     def __init__(self, music, food , movie , habbit , country , animal , singer , thing1 , thing2):
+#         self.driver = webdriver.Chrome()
 
 
 
-
-
-my_gift = BirthdayGift(music, food , movie , habbit , country , animal , singer , thing1 , thing2)
+#my_gift = BirthdayGift(music, food , movie , habbit , country , animal , singer , thing1 , thing2)
 
 #finally i will get last time
 now = datetime.now()
